d7a96008a8fab7327807596da7a0ef24ac4023a7
Kambiz Moradi <kambiz.moradi.9103653@gmail.com>
2021-06-06 22:16:42 +0430

    Patch: underlinetags

--- config.def.h
+++ config.def.h
@@ -27,7 +27,7 @@ static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { "#ffffff", "#000000", "#000000" },
     [SchemeInv]  = { "#ffffff", "#000000", "#000000" },
-	[SchemeSel]  = { "#ffffff", "#003fff", "#000000" },
+	[SchemeSel]  = { "#ffffff", "#000000", "#000000" },
 };
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
@@ -38,6 +38,11 @@ static const unsigned int alphas[][3]      = {
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+static const unsigned int ulinepad	= 5;	/* horizontal padding between the underline and tag */
+static const unsigned int ulinestroke	= 2;	/* thickness / height of the underline */
+static const unsigned int ulinevoffset	= 0;	/* how far above the bottom of the bar the line should appear */
+static const int ulineall 		= 0;	/* 1 to show underline on all tags, 0 for just the active ones */
 
 static const Rule rules[] = {
 	/* xprop(1):
--- config.h
+++ config.h
@@ -5,10 +5,10 @@
 #include "fibonacci.c" /* for fibonachi patch*/
 #include "tatami.c" /* for tatami patch*/
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 15;       /* gaps between windows */
-static const unsigned int igappx    = 15;        /* size of inner gaps */
-static const unsigned int ogappx    = 15;        /* size of outer gaps */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int gappx     = 4;       /* gaps between windows */
+static const unsigned int igappx    = 4;        /* size of inner gaps */
+static const unsigned int ogappx    = 4;        /* size of outer gaps */
 static const int gapsforone	    = 0;	/* 1 enable gaps when only one window is open */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
@@ -21,27 +21,37 @@ static const char col_gray2[]       = "#ffffff";
 static const char col_gray3[]       = "#ffffff";
 static const char col_gray4[]       = "#ffffff";
 static const char col_cyan[]        = "#0066ff";
+static const unsigned int baralpha = 0x99;
+static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-    [SchemeInv]  = { col_gray1, col_gray3, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  "#000075" },
+	[SchemeNorm] = { "#ffffff", "#000000", "#000000" },
+    [SchemeInv]  = { "#ffffff", "#000000", "#000000" },
+	[SchemeSel]  = { "#ffffff", "#000000", "#000000" },
+};
+static const unsigned int alphas[][3]      = {
+	/*               fg      bg        border     */
+	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
+	[SchemeInv]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+static const unsigned int ulinepad	= 5;	/* horizontal padding between the underline and tag */
+static const unsigned int ulinestroke	= 2;	/* thickness / height of the underline */
+static const unsigned int ulinevoffset	= 0;	/* how far above the bottom of the bar the line should appear */
+static const int ulineall 		= 0;	/* 1 to show underline on all tags, 0 for just the active ones */
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-    /* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-    { "st-256color", NULL,       NULL,       0,            0,           1,          0,         -1 },
-    { "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-    { "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-    { "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-    { NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+    /* class                instance    title           tags mask   isfloating  isterminal  noswallow   monitor */
+    { "st-256color",        NULL,       NULL,           0,          0,          1,          0,          -1 },
+    { NULL,                 NULL,       "Event Tester", 0,          0,          0,          1,          -1 }, /* xev */
 };
 
 /* layout(s) */
@@ -52,15 +62,15 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
-	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	/* symbol     arrange function  (first entry is default) */
 	{ "[M]",      monocle },
+	{ "<||",      tile },    
     { "|M|",      centeredmaster },
-	{ ">M>",      centeredfloatingmaster },
+	{ "<M>",      centeredfloatingmaster },
     { "[@]",      spiral },
  	{ "[\\]",      dwindle },
     { "|+|",      tatami },
+	{ "><>",      NULL },    /* no layout function means floating behavior */
     { NULL,       NULL },
 };
 
@@ -89,26 +99,27 @@ static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	/*{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },*//* use sxhkd*/
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
     { MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = -1 } },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_Tab,    focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-    { MODKEY|Mod4Mask,              XK_r,      reorganizetags, {0} },
+    { MODKEY,                       XK_grave,  reorganizetags, {0} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ Mod4Mask,                     XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY|ShiftMask,             XK_i,      setigaps,       {.i = +5 } },
-	{ MODKEY|ControlMask,           XK_i,      setigaps,       {.i = -5 } },
+	{ MODKEY|ShiftMask,             XK_i,      setigaps,       {.i = +2 } },
+	{ MODKEY|ControlMask,           XK_i,      setigaps,       {.i = -2 } },
 	{ MODKEY|ShiftMask|ControlMask, XK_i,      setigaps,       {.i = 0  } },
-	{ MODKEY|ShiftMask,             XK_o,      setogaps,       {.i = +5 } },
-	{ MODKEY|ControlMask,           XK_o,      setogaps,       {.i = -5 } },
+	{ MODKEY|ShiftMask,             XK_o,      setogaps,       {.i = +2 } },
+	{ MODKEY|ControlMask,           XK_o,      setogaps,       {.i = -2 } },
 	{ MODKEY|ShiftMask|ControlMask, XK_o,      setogaps,       {.i = 0  } },
     { MODKEY|ShiftMask,             XK_x,      killunsel,      {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
@@ -116,6 +127,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
     { MODKEY,                       XK_u,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
+    { MODKEY|ControlMask,   		XK_z,      cyclelayout,    {.i = -1 } },
     { MODKEY|ControlMask,   		XK_comma,  cyclelayout,    {.i = -1 } },
 	{ MODKEY|ControlMask,           XK_period, cyclelayout,    {.i = +1 } },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
@@ -130,12 +142,12 @@ static Key keys[] = {
     { MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_z,      focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_z,      tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-    { MODKEY,                       XK_minus,  setgaps,        {.i = -5 } },
-    { MODKEY,                       XK_equal,  setgaps,        {.i = +5 } },
+    { MODKEY,                       XK_minus,  setgaps,        {.i = -2 } },
+    { MODKEY,                       XK_equal,  setgaps,        {.i = +2 } },
     { MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
@@ -155,8 +167,8 @@ static Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	/* { ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} }, */
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
