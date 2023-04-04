9a3c012c4b1670c0292deeeec406d7b0c41113b5
Kambiz Moradi <kambiz.moradi.9103653@gmail.com>
2023-04-04 13:20:16 +0330

    Patch: dwm-rotatetags-20210723-cb3f58a

--- config.def.h
+++ config.def.h
@@ -104,7 +104,9 @@ static const Key keys[] = {
    { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
    { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
    { MODKEY|ControlMask,           XK_j,      pushdown,       {0} },
-	{ MODKEY|ControlMask,           XK_k,      pushup,         {0} },
+   { MODKEY|ControlMask,           XK_k,      pushup,         {0} },
+   { MODKEY,                       XK_Left,   rotatetags,     {.i = -1 } },
+   { MODKEY,                       XK_Right,  rotatetags,     {.i = +1 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
--- dwm.c
+++ dwm.c
@@ -215,6 +215,7 @@ static void resize(Client *c, int x, int y, int w, int h, int interact);
 static void resizeclient(Client *c, int x, int y, int w, int h);
 static void resizemouse(const Arg *arg);
 static void restack(Monitor *m);
+static void rotatetags(const Arg *arg);
 static void rotatestack(const Arg *arg);
 static void run(void);
 static void scan(void);
@@ -1583,6 +1584,34 @@ restack(Monitor *m)
 	if (m == selmon && (m->tagset[m->seltags] & m->sel->tags) && selmon->lt[selmon->sellt] != &layouts[2])
 		warp(m->sel);
 }
+
+void
+rotatetags(const Arg *arg)
+{
+	const int rot = abs(arg->i);
+	const unsigned int tagset = selmon->tagset[selmon->seltags];
+	unsigned int newtagset;
+
+	/* check the direction of the shift */
+	if (arg->i < 0) {
+		 /* shift tags right */
+		 newtagset = (tagset >> rot) | (tagset << (LENGTH(tags) - rot));
+	} else {
+		 /* shift tags left */
+		 newtagset = (tagset << rot) | (tagset >> (LENGTH(tags) - rot));
+	}
+
+	/* mask the tag bits */
+	newtagset &= TAGMASK;
+
+	if (newtagset) {
+		 selmon->tagset[selmon->seltags] = newtagset;
+		 focus(NULL);
+		 arrange(selmon);
+	}
+}
+
+
 
 void
 rotatestack(const Arg *arg)
