232f5e61425a1187a36747e975c6c1755cfe0115
Kambiz Moradi <kambiz.moradi.9103653@gmail.com>
2023-04-06 09:08:48 +0330

    Patch: dwm-centeredmaster-6.1

--- config.def.h
+++ config.def.h
@@ -49,6 +49,8 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
 	{ NULL,       NULL },
 };
 
@@ -93,6 +95,8 @@ static const Key keys[] = {
 	{ MODKEY,					XK_t,				setlayout,				{.v = &layouts[0]} },
 	{ MODKEY,					XK_f,				setlayout,				{.v = &layouts[1]} },
 	{ MODKEY,					XK_m,				setlayout,				{.v = &layouts[2]} },
+	{ MODKEY,					XK_u,				setlayout,				{.v = &layouts[3]} },
+	{ MODKEY,					XK_o,				setlayout,				{.v = &layouts[4]} },
 	{ MODKEY|ControlMask,		XK_comma,			cyclelayout,			{.i = -1 } },
 	{ MODKEY|ControlMask,		XK_z,				cyclelayout,			{.i = -1 } },
 	{ MODKEY|ControlMask,		XK_period,			cyclelayout,			{.i = +1 } },
